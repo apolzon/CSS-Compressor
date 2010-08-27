f6fc8ae95b1a64552bd03857a9d03429262a3ea2
Chris Apolzon <apolzon@gmail.com>
2010-08-27 11:54:38 -0700

    raising if file doesn't exist

--- css_compressor.rb
+++ css_compressor.rb
@@ -1,10 +1,11 @@
 class CssCompressor
 	def initialize(file)
+		raise "File not found" unless File.file?(file)
 		@file = File.open(file, 'r')
 	end
 
 	def compress_to(destination)
-		@output = File.open(destination, 'w')
+		@output = File.new(destination, 'w')
 		# Parse file
 		# Remove all /* */ lines
 		# Remove all blank lines
